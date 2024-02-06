aa1c65f48eec370a018bab7611de14a4c3549f0a
DemonHashira <v.logodazhki@outlook.com>
2024-02-06 11:53:40 +0200

    Added Places Screen

---
+++ lib/screens/places.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:places_app/widgets/places_list.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: PlacesList(
+        places: [],
+      ),
+    );
+  }
+}
