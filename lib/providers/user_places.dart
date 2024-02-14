c26c3191156a251ec957bc3fc96ae9e8a4ba4e0f
DemonHashira <v.logodazhki@outlook.com>
2024-02-14 19:35:33 +0200

    Slight changes on the provider

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:places_app/models/place.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:sqflite/sqlite_api.dart';
+
+import 'package:places_app/models/place.dart';
 
 Future<Database> _getDatabase() async {
   final dbPath = await sql.getDatabasesPath();
@@ -14,8 +14,7 @@ Future<Database> _getDatabase() async {
     path.join(dbPath, 'places.db'),
     onCreate: (db, version) {
       return db.execute(
-        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)',
-      );
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
     },
     version: 1,
   );
@@ -33,9 +32,7 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
           (row) => Place(
             id: row['id'] as String,
             title: row['title'] as String,
-            image: File(
-              row['image'] as String,
-            ),
+            image: File(row['image'] as String),
             location: PlaceLocation(
               latitude: row['lat'] as double,
               longitude: row['lng'] as double,
@@ -50,8 +47,8 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
-    final fileName = path.basename(image.path);
-    final copiedImage = await image.copy('${appDir.path}/$fileName');
+    final filename = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$filename');
 
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
